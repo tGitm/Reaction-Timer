0a006ccb7a643bab1610eb4373b45aad23f72849
tGitm <timrus99@gmail.com>
2023-02-13 12:35:38 +0100

    Completed timer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>Check your reflexes</h1>
+	<button @click="start" :disabled="isPlaying">Play</button>
+
+	<Block v-if="isPlaying" :delay="delay" @react="endGame" />
+	
+	<Result v-if="showResults" :reactionTimeScore="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {Block, Result },
+	data() {
+		return {
+			isPlaying: false,
+			delay: null,
+			score: null,
+			showResults: false
+		}
+	},
+	methods: {
+		start() {
+			this.delay = 2000 + Math.random() + 5000 //random amount between 0 and 5000ms
+			this.isPlaying = true
+			this.showResults = false
+		},
+
+		endGame(reactionTime) {
+			this.score = reactionTime
+			this.isPlaying = false //end the game
+			this.showResults = true
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #444;
+		margin-top: 60px;
+	}
+
+	button {
+		background: #0faf87;
+		color: white;
+		border: solid 1px #0faf87;
+		padding: 8px 16px;
+		border-radius: 4px;
+		box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+		font-size: 16px;
+		letter-spacing: 1px;
+		cursor: pointer;
+		margin: 10px;
+		transition: 0.3s ease;
+  	}
+
+	button:hover {
+		background: #fff;
+		color: #0faf87;
+	}
+
+	button[disabled] {
+		opacity: 0.2;
+		cursor: not-allowed;
+	}
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,52 @@
+<template>
+    <br> <br> <br>
+    <h3>Your reaction time is {{ reactionTimeScore }} ms.</h3>
+    <p :class="{ 'good': time === 1, 'slow': time === 2, 'bad': time === 3 }"> {{ resutlText }}</p>
+</template>
+
+<script>
+export default {
+    props: ['reactionTimeScore'],
+    data() {
+        return {
+            resutlText: null,
+            time: null
+        }
+    },
+    methods: {
+        appResponse() {
+            if (this.reactionTimeScore < 300) {
+                this.resutlText = "Good :)"
+                this.time = 1
+            } else if (this.reactionTimeScore < 500) {
+                this.resutlText = "Slow!"
+                this.time = 2
+            } else {
+                this.resutlText = "Very bad :("
+                this.time = 3
+            }
+        }
+    },
+    mounted() {
+        this.appResponse()
+    }
+}
+</script>
+
+<style>
+    h3 {
+        color: #42b883;
+    }
+
+    .good {
+        color:#42b883
+    }
+
+    .slow {
+        color: orange;
+    }
+
+    .bad {
+        color: red;
+    }
+</style>
